05d0fc031ab17759b2fdb62db95be5b29b710c1c
OverSoul404 <souloms@mail.ru>
2020-09-28 20:59:01 +0300

    Курс нам и нахуй не нужон, так что я его убрал блять

--- vk_bot.py
+++ vk_bot.py
@@ -4,7 +4,7 @@ from currency import Currency
 class VkBot:
     
     def __init__(self):
-        self._COMMANDS = [['что умеешь', 'хелп', 'помощь', 'help'], ['курс', 'валюта',]]
+        self._COMMANDS = [['что умеешь', 'хелп', 'помощь', 'help']]
 
     def new_message(self, message):
         '''Чекер сообщений
@@ -18,22 +18,4 @@ class VkBot:
             return '''Краткий экскурс по командам:
             /хелп - выводит 
             /курс - выводит курсы валют относительно одного доллара США по данным Google
-            '''
-
-
-        if message.lower() in self._COMMANDS[1]:
-
-            cur = Currency()
-
-            return f'''
-            Курс валют на {cur.TIME}:
-            
-            Российский рубль - {cur.RUB} ₽
-            Белорусский рубль - {cur.BYN} Br
-            Евро - {cur.EUR} €
-            Биткоин - {cur.BTC} ₿
-            Республиканский стафил - {cur.RSF} ŘŞ
-            '''
-
-
-        
+            '''       
